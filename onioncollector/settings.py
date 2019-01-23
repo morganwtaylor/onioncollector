09db6f715d3df770cbb10ae4639f1967c73c1bb4
morganwtaylor <morgan@morganwtaylor.com>
2019-01-23 14:45:27 -0700

    changed database backend to sqllite

--- onioncollector/settings.py
+++ onioncollector/settings.py
@@ -82,14 +82,10 @@ WSGI_APPLICATION = 'onioncollector.wsgi.application'
 
 DATABASES = {
     	'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'onioncollector',
-        	    'USER':'onioncollector',
-        	    'PASSWORD':'testpassword',
-        	    'HOST': 'localhost',
-        	    'PORT': '',
-             }
-         }
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
